580cbe316533d60d32b51e071eb79ae2c7a6f41a
Carl Ellis <carlc75@gmail.com>
2018-11-29 11:42:17 +0000

    Topic endpoint working

--- service.py
+++ service.py
@@ -1,9 +1,11 @@
+import json
 import logging
 import os
 
 from aiohttp import web
 
 import gensim
+import numpy
 import tomodachi
 
 
@@ -15,6 +17,18 @@ LDA_FILE = os.environ.get("TOPIC_LDA_FILE")
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 
+class NumPyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, numpy.integer):
+            return int(obj)
+        elif isinstance(obj, numpy.floating):
+            return float(obj)
+        elif isinstance(obj, numpy.ndarray):
+            return obj.tolist()
+        else:
+            return super(NumPyEncoder, self).default(obj)
+
+
 @tomodachi.service
 class TopicService(tomodachi.Service):
 
@@ -23,6 +37,7 @@ class TopicService(tomodachi.Service):
     lda = gensim.models.LdaModel.load(LDA_FILE)
 
     name = "topic_service"
+    log_level = 'INFO'
 
     options = {
         'http': {
@@ -37,6 +52,19 @@ class TopicService(tomodachi.Service):
         return "topic"
 
     @tomodachi.http("POST", r"/topic/?")
-    def topic():
-        pass
+    async def topic(self, request: web.Request) -> str:
+        data = await request.json()
+        text = data.get("text", "")
+
+        # Process text to BOW
+        doc = gensim.utils.simple_tokenize(text)
+        bow = TopicService.id2word.doc2bow(list(doc))
+
+        doc_lda = TopicService.lda[bow]
+
+        # Extract Topics
+        topics = sorted(doc_lda, key=lambda x: x[1], reverse=True)
+        topic_data = [(t[0], t[1], TopicService.lda.show_topic(t[0])) for t in topics]
+
+        return tomodachi.HttpResponse(body=json.dumps({"data": topic_data}, cls=NumPyEncoder), status=200, content_type='application/json')
 
